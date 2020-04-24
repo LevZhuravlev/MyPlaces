e86f207e491ea02f903489deb817a4f830064e71
Biiil <leoalexShuravl@gmail.com>
2020-04-24 12:18:20 +0300

    first preparation's for rating control

--- MyPlaces/RatingControl.swift
+++ MyPlaces/RatingControl.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class RatingControl: UIStackView {
 
+    
+
+    
     // MARK: Initialization
     
     // теперь объявляем инициализаторы
@@ -24,19 +27,57 @@ class RatingControl: UIStackView {
         // родительского класса
         
         super.init(frame: frame)
+        setupButtons()
+
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
     }
+    
+    // MARK: Button action
+    
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("ButtonPressed 👍")
+        
+    }
+    
     
     // MARK: Private Methods
     
     private func setupButtons() {
+           
+    // Сreate the button
+        
+        let button = UIButton()
+        button.backgroundColor = .red
+     
+    // Add constraints for button
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+            // отключает автоматически сгенерированные констрейнты для кнопки,
+            // потому что если создавать объект программно, то его свойство
+            // по умолчанию автоматически генерирует констрейнты
         
         
-        
-    }
-}
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
+            // строки определяющие высоту и ширину кнопки
 
+    // Setup button action
+        
+        button.addTarget(self, // показываем что действие будет выполняться из класса
+                         action: #selector(ratingButtonTapped(button:)), // действие
+                         for: .touchUpInside) // событие по которому будет срабатывать вызов метода
+        
 
+        
+    // Add button to the stackView
+        
+        addArrangedSubview(button)
+        // это свойство добавляет кнопку в список представлений,
+        // как subView класса ratingControl
+       }
+   }
+ 
