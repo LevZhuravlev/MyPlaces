db3f79bb1297c4a270416a534ef7690a6e676774
Biiil <leoalexShuravl@gmail.com>
2020-05-18 11:35:56 +0300

    prepare to get posstion

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -10,22 +10,31 @@ import UIKit
 import MapKit
 import CoreLocation
 
+protocol MapViewControllerDelegate {
+    func getAddress(_ address: String?)
+}
+
 class MapViewController: UIViewController {
     
     var place = Place()
+    var annotationIdentefier = "annotationIdentefier"
     var locationManager = CLLocationManager()
     var incomeSegueIdentefier = ""
+    
+    
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var locationButton: UIButton!
     @IBOutlet weak var mapPinImage: UIImageView!
-    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addressLabel.text = ""
         setupMapView()
         checkLocationAutoriztion()
+        mapView.delegate = self
         if (locationManager.location?.coordinate) != nil {}
 
     }
@@ -229,10 +238,26 @@ class MapViewController: UIViewController {
         if incomeSegueIdentefier == "showPlace" {
             setupPlacemark()
             mapPinImage.isHidden = true
-            adressLabel.isHidden = true
+            addressLabel.isHidden = true
             doneButton.isHidden = true
         }
         else if incomeSegueIdentefier == "getAdress" {showUserLocation()}
+    }
+    
+    // метод определяющий координаты центра экрана
+    private func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        
+        // для определения координат надо знать широту и долготу
+        let latitude = mapView.centerCoordinate.latitude // широта
+        let longitude = mapView.centerCoordinate.longitude // долгота
+        
+        // нам необходимо вернуть координаты конкретной точки
+        // поэтому инициализируем
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+        
+        // теперь у нас есть метод который возвращает координаты
+        // точки находящиеся по центру экрана%
     }
     
     // кнопка закрытия окна
@@ -254,4 +279,93 @@ extension MapViewController:CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAutoriztion()
     }
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+    // метод который вызывается при смене отображаемого региона
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        
+        // сначала определем текущие координаты по центру отображаемой области
+        let center = getCenterLocation(for: mapView)
+        
+        // класс отвечающий за преобразование координат
+        let geocoder = CLGeocoder()
+        
+        // вызываем метод класса CLGeocoder()
+        // который будет преобразовывать
+        // координаты в адрес
+        // на вход метода подаются координаты
+        // а на выходе получаем массив меток
+        // или ошибку
+        
+        geocoder.reverseGeocodeLocation(center) { (placemarks, error) in
+            
+            // проверяем на наличие ошибок
+            if let error = error {print("error"); return}
+            
+            // если ошибки нет, то нам нужно
+            // извлечь массив меток
+            
+            guard let placemarks = placemarks else {return}
+            
+            // извлекаем первую метку
+            let placemark = placemarks.first
+            
+            // теперь нам надо извлечь улицу и номер дома
+            let streetName = placemark?.thoroughfare // улица
+            let buildNumber = placemark?.subThoroughfare // дом
+            
+             // обнавлять интерфейс надо в основном потоке ассинхронно
+            DispatchQueue.main.async {
+                
+                // значения дома и улицы будут опционпльными так как не всегда их можно определить по локации
+                // поэтому прежде чем передать в лейбл название удицы и дома надо выполнить проверки
+                
+                if streetName != nil && buildNumber != nil {
+                 self.addressLabel.text = "\(streetName!), \(buildNumber!)"
+                }
+                else if streetName != nil  {self.addressLabel.text = "\(streetName!)"}
+                else {self.addressLabel.text = ""}
+           
+        }
+    }
+    
+    // метод отвечающий за отображение аннотации
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else {return nil}
+        
+        // далее создаем объект, который в последствии и будет возвращаться
+        // и который и будет представлять из себя view с аннотацией на карте
+        // чтобы не создавать новое представление при вызове данного метода
+        // лучше переиспользовать ранее созданные аннотации этого типа
+        // для этого мы создали annotationIdentefier
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentefier) as? MKPinAnnotationView
+        
+        // в случае если на карте не окажется ни одного представления
+        // с аннотацией которое мы могли бы переиспользовать
+        if annotationView == nil {
+            
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentefier)
+            
+            // для того чтобы использовать аннотацию в виде банера
+            annotationView?.canShowCallout = true
+            
+        }
+        
+        // Изображение для аннотации
+        // Делаем проверку на то что изображение есть
+        if let imageData =  place.imageData {
+            
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+              imageView.layer.cornerRadius = 10
+              imageView.clipsToBounds = true
+              imageView.image  = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        
+        return annotationView
+    }
+}
 }
